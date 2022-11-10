1c508d31d91bab4f2e3d3366312b8e6bd8f251ea
Juyoung Jin <jinjuyoung05207@gmail.com>
2022-11-10 23:04:32 +0900

    feat: 네비게이션 설정, 바텀 네비게이션 연결

--- presentation/src/main/java/com/gta/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/gta/presentation/ui/MainActivity.kt
@@ -2,11 +2,22 @@ package com.gta.presentation.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.gta.presentation.R
+import com.gta.presentation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fcv_home_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnvHomeBottomNav.setupWithNavController(navController)
     }
 }
---
+++ presentation/src/main/java/com/gta/presentation/ui/map/MapFragment.kt
@@ -0,0 +1,18 @@
+package com.gta.presentation.ui.map
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.gta.presentation.R
+
+class MapFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+}
