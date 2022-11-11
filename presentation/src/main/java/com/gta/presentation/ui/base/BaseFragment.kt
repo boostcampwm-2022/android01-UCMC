cac2afef5002aecf525060392f2be748669ca2a5
minseonglove <qnfpdlr124@naver.com>
2022-11-11 19:41:29 +0900

    feat : BaseFragment 추가

---
+++ presentation/src/main/java/com/gta/presentation/ui/base/BaseFragment.kt
@@ -0,0 +1,35 @@
+package com.gta.presentation.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<VB : ViewDataBinding>(
+    @LayoutRes private val layoutRes: Int
+) : Fragment() {
+
+    private var _binding: VB? = null
+    protected val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        return binding.run {
+            lifecycleOwner = this@BaseFragment
+            root
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
