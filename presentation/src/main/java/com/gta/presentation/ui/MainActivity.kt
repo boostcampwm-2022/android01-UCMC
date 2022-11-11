0ab5396a17b48bf4b728edc91898e388751e4822
Juyoung Jin <jinjuyoung05207@gmail.com>
2022-11-11 17:12:31 +0900

    refactor: binding 전역화 및 리뷰 반영

--- presentation/src/main/java/com/gta/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/gta/presentation/ui/MainActivity.kt
@@ -2,18 +2,17 @@ package com.gta.presentation.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.gta.presentation.R
 import com.gta.presentation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(binding.root)
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fcv_home_fragment) as NavHostFragment
