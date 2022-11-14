7a9b7e3896a1a4b31a51e1cdc1d5ade708b047ff
hoon <ldh019@naver.com>
2022-11-14 18:34:08 +0900

    feat : 위치 권한 추가 및 지도 위치 설정

--- presentation/src/main/java/com/gta/presentation/ui/map/MapFragment.kt
+++ presentation/src/main/java/com/gta/presentation/ui/map/MapFragment.kt
@@ -1,17 +1,42 @@
 package com.gta.presentation.ui.map
 
+import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.registerForActivityResult
 import androidx.annotation.UiThread
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
 import com.gta.presentation.R
 import com.gta.presentation.databinding.FragmentMapBinding
 import com.gta.presentation.ui.base.BaseFragment
-import com.naver.maps.map.MapView
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.*
+import com.naver.maps.map.util.FusedLocationSource
 
 class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map), OnMapReadyCallback {
     private lateinit var mapView: MapView
+    private lateinit var naverMap: NaverMap
+    private lateinit var locationSource: FusedLocationSource
+    private val activityResultLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { resultMap ->
+            val isAllGranted = permissions.all { e -> resultMap[e] == true }
+
+            if (isAllGranted) {
+                if (!locationSource.isActivated) {
+                    naverMap.locationTrackingMode = LocationTrackingMode.None
+                } else {
+                    naverMap.locationTrackingMode = LocationTrackingMode.Follow
+                }
+            }
+        }
+
+    private val permissions = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -19,6 +44,21 @@ class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map), OnM
         mapView.onCreate(savedInstanceState)
 
         mapView.getMapAsync(this)
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+        naverMap.locationSource = locationSource
+        activityResultLauncher.launch(permissions)
+
+        val setting = naverMap.uiSettings
+
+        with(setting) {
+            isCompassEnabled = true
+            isScaleBarEnabled = true
+            isLocationButtonEnabled = true
+        }
     }
 
     override fun onStart() {
@@ -56,15 +96,7 @@ class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map), OnM
         mapView.onLowMemory()
     }
 
-    @UiThread
-    override fun onMapReady(map: NaverMap) {
-        val setting = map.uiSettings
-
-        with(setting) {
-            isCompassEnabled = true
-            isScaleBarEnabled = true
-            isLocationButtonEnabled = true
-        }
-
+    companion object {
+        const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
