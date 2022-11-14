ac224d32c26478d6f6917ccf1263cdced3b91a32
Hyunji99Choi <tyg06229@gmail.com>
2022-11-14 22:50:52 +0900

    feat: 현재차량 정보를 가져오기 위한 UseCase

---
+++ domain/src/main/java/com/gta/domain/usecase/cardetail/GetCarDetailDataUseCase.kt
@@ -0,0 +1,14 @@
+package com.gta.domain.usecase.cardetail
+
+import com.gta.domain.model.CarDetail
+import com.gta.domain.repository.CarRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetCarDetailDataUseCase @Inject constructor(
+    private val carRepository: CarRepository
+) {
+    operator fun invoke(carId: String): Flow<CarDetail> {
+        return carRepository.getCarData(carId)
+    }
+}
