31df84e55b630d26a54031f92a95fce9776ab366
Hoon <ldh019@naver.com>
2022-11-14 14:07:28 +0900

    feat : map fragment 상속 변경

--- presentation/src/main/java/com/gta/presentation/ui/map/MapFragment.kt
+++ presentation/src/main/java/com/gta/presentation/ui/map/MapFragment.kt
@@ -4,16 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.gta.presentation.R
+import com.gta.presentation.databinding.FragmentMapBinding
+import com.gta.presentation.ui.base.BaseFragment
 
-class MapFragment : Fragment() {
+class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_map, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
     }
 }
