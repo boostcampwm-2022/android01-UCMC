83c86b457aa5c90de26dc06e4be035947983e38f
Juyoung Jin <jinjuyoung05207@gmail.com>
2022-11-15 14:47:31 +0900

    design: 예약하기 화면 ui

---
+++ presentation/src/main/java/com/gta/presentation/ui/map/ReservationFragment.kt
@@ -0,0 +1,9 @@
+package com.gta.presentation.ui.map
+
+import com.gta.presentation.R
+import com.gta.presentation.databinding.FragmentReservationBinding
+import com.gta.presentation.ui.base.BaseFragment
+
+class ReservationFragment : BaseFragment<FragmentReservationBinding>(R.layout.fragment_reservation) {
+
+}
