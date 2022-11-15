5508de1a1cccd63301e497d9783622208e0fdc53
Juyoung Jin <jinjuyoung05207@gmail.com>
2022-11-16 02:27:28 +0900

    feat: ViewModel 생성, 날짜 양방향 데이터 바인딩

--- presentation/src/main/java/com/gta/presentation/ui/BindingAdapter.kt
+++ presentation/src/main/java/com/gta/presentation/ui/BindingAdapter.kt
@@ -1,1 +1,15 @@
 package com.gta.presentation.ui
+
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+@BindingAdapter("selection")
+fun setReservationTime(textView: TextView, selection: Pair<Long, Long>?) {
+    selection ?: return
+    val dateFormat = SimpleDateFormat("yy/MM/dd", Locale.getDefault())
+    textView.text = "${dateFormat.format(selection.first)} ~ ${dateFormat.format(selection.second)}"
+}
+
