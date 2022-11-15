81be25e701afc74111f975db8355d6b1259d9898
Juyoung Jin <jinjuyoung05207@gmail.com>
2022-11-15 12:24:25 +0900

    feat: 바텀 네비게이션 visibility 설정, 툴바 네비게이션 연결

--- presentation/src/main/java/com/gta/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/gta/presentation/ui/MainActivity.kt
@@ -1,19 +1,50 @@
 package com.gta.presentation.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.gta.presentation.R
 import com.gta.presentation.databinding.ActivityMainBinding
 import com.gta.presentation.ui.base.BaseActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+    private val navHostFragment by lazy { supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment }
+    private val navController by lazy { navHostFragment.navController }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.fcv_home_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        binding.bnvHomeBottomNav.setupWithNavController(navController)
+
+        setSupportActionBar(binding.tbMain)
+
+        setupWithBottomNavigation()
+
+        setupWithAppBar()
+    }
+
+    private fun setupWithAppBar() {
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.mapFragment, R.id.chattingFragment, R.id.myPageFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    private fun setupWithBottomNavigation() {
+        binding.bnvMain.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.mapFragment, R.id.chattingFragment, R.id.myPageFragment -> showBottomNav()
+                else -> hideBottomNav()
+            }
+        }
+    }
+
+    private fun showBottomNav() {
+        binding.bnvMain.visibility = View.VISIBLE
+    }
+
+    private fun hideBottomNav() {
+        binding.bnvMain.visibility = View.GONE
     }
 }
