ab550f2708535e8dd82454a376fee81d46e3c410
minseonglove <qnfpdlr124@naver.com>
2022-11-15 16:38:46 +0900

    feat : 유저 정보 데이터 클래스 추가

---
+++ data/src/main/java/com/gta/data/model/DrivingLicense.kt
@@ -0,0 +1,9 @@
+package com.gta.data.model
+
+data class DrivingLicense(
+    val id: String,
+    val name: String,
+    val residentNumber: String,
+    val aptitudeTestDate: String,
+    val expireDate: String,
+)
