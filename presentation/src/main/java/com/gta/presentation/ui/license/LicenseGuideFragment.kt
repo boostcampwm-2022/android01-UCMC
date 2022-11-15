b68afaeb4584e37633ff6eb937b53fe200527273
minseonglove <qnfpdlr124@naver.com>
2022-11-16 02:00:40 +0900

    feat : 카메라 촬영 기능 추가

--- presentation/src/main/java/com/gta/presentation/ui/license/LicenseGuideFragment.kt
+++ presentation/src/main/java/com/gta/presentation/ui/license/LicenseGuideFragment.kt
@@ -2,13 +2,16 @@ package com.gta.presentation.ui.license
 
 import android.Manifest
 import android.os.Bundle
+import android.os.Environment
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
 import androidx.core.content.PermissionChecker.checkCallingOrSelfPermission
 import com.gta.presentation.R
 import com.gta.presentation.databinding.FragmentLicenseGuideBinding
 import com.gta.presentation.ui.base.BaseFragment
+import java.io.File
 
 class LicenseGuideFragment : BaseFragment<FragmentLicenseGuideBinding>(
     R.layout.fragment_license_guide
@@ -20,6 +23,29 @@ class LicenseGuideFragment : BaseFragment<FragmentLicenseGuideBinding>(
                 takePicture()
             }
         }
+
+    private val cameraLauncher =
+        registerForActivityResult(ActivityResultContracts.TakePicture()) { isSucceed ->
+            if (isSucceed) {
+                // photoUri
+            }
+        }
+
+    private val photoFile by lazy {
+        File.createTempFile(
+            "IMG_",
+            ".jpg",
+            requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        )
+    }
+
+    private val photoUri by lazy {
+        FileProvider.getUriForFile(
+            requireContext(),
+            "${requireContext().packageName}.provider",
+            photoFile
+        )
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -38,6 +64,6 @@ class LicenseGuideFragment : BaseFragment<FragmentLicenseGuideBinding>(
     }
 
     private fun takePicture() {
-
+        cameraLauncher.launch(photoUri)
     }
 }
