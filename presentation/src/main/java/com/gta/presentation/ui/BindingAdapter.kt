c0d46beb1a55e828453ca733d9250164547ac030
Hyunji99Choi <tyg06229@gmail.com>
2022-11-15 16:41:12 +0900

    REBASE string.xml 충돌 해결

--- presentation/src/main/java/com/gta/presentation/ui/BindingAdapter.kt
+++ presentation/src/main/java/com/gta/presentation/ui/BindingAdapter.kt
@@ -1,1 +1,9 @@
 package com.gta.presentation.ui
+
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("car_type", "car_title")
+fun setCarDetailTitle(textView: TextView, type: String, title: String) {
+    textView.text = "[$type] $title"
+}
