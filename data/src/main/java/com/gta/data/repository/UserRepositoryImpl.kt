8b50204505d5e57c8f35cecd7a75cd602d8db7c3
Hyunji99Choi <tyg06229@gmail.com>
2022-11-15 17:23:08 +0900

    style : user Repository 함수 정리(삭제)

--- data/src/main/java/com/gta/data/repository/UserRepositoryImpl.kt
+++ data/src/main/java/com/gta/data/repository/UserRepositoryImpl.kt
@@ -1,16 +1,9 @@
 package com.gta.data.repository
 
-import com.gta.domain.model.UserProfile
 import com.gta.domain.repository.UserRepository
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
 
 class UserRepositoryImpl : UserRepository {
     override fun getMyUserId(): String {
         return "(test)myId"
-    }
-
-    override fun getUserProfile(userId: String): Flow<UserProfile> {
-        return MutableStateFlow(UserProfile(userId, "(test)$userId", 36.5F, null))
     }
 }
--- domain/src/main/java/com/gta/domain/repository/UserRepository.kt
+++ domain/src/main/java/com/gta/domain/repository/UserRepository.kt
@@ -1,9 +1,5 @@
 package com.gta.domain.repository
 
-import com.gta.domain.model.UserProfile
-import kotlinx.coroutines.flow.Flow
-
 interface UserRepository {
     fun getMyUserId(): String
-    fun getUserProfile(userId: String): Flow<UserProfile>
 }
