7f083d855541bfeaf91dfc5b7282d4ca23f50bfe
Juyoung Jin <jinjuyoung05207@gmail.com>
2022-11-16 02:23:48 +0900

    feat: 예약하기 화면 navigation graph에 추가, argument 생성

--- presentation/src/main/java/com/gta/presentation/ui/Reservation/ReservationFragment.kt
+++ presentation/src/main/java/com/gta/presentation/ui/Reservation/ReservationFragment.kt
@@ -1,28 +1,31 @@
 package com.gta.presentation.ui.reservation
 
+import androidx.navigation.fragment.navArgs
 import com.google.android.material.datepicker.CalendarConstraints
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.gta.presentation.R
 import com.gta.presentation.databinding.FragmentReservationBinding
 import com.gta.presentation.ui.base.BaseFragment
 import com.gta.presentation.util.DateValidator
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.parcelize.Parcelize
 import timber.log.Timber
 
 class ReservationFragment :
     BaseFragment<FragmentReservationBinding>(R.layout.fragment_reservation) {
+    private val args: ReservationFragmentArgs by navArgs()
+    private val carInfo by lazy { args.carInfo }
 
     override fun onStart() {
         super.onStart()
 
-        // 임시 대여 가능 날짜
-        val validRange = Pair(1668438000000, 1669593600000)
         // 임시 불가능한 날짜들
         val invalidateDates = listOf(1669248000000 to 1669420800000)
 
         val constraints = CalendarConstraints.Builder()
-            .setValidator(DateValidator(validRange, invalidateDates))
-            .setStart(validRange.first)
-            .setEnd(validRange.second)
+            .setValidator(DateValidator(carInfo.reservationRange, invalidateDates))
+            .setStart(carInfo.reservationRange.first)
+            .setEnd(carInfo.reservationRange.second)
             .build()
 
         val datePicker = MaterialDatePicker.Builder
@@ -40,3 +43,16 @@ class ReservationFragment :
         }
     }
 }
+
+// 임시 자동차 객체
+@Parcelize
+data class TmpCarInfo(
+    val id: String,
+    val title: String,
+    val location: String,
+    val carType: String,
+    val price: Int,
+    val comment: String,
+    val images: List<String>,
+    val reservationRange: Pair<Long, Long>
+) : Parcelable
