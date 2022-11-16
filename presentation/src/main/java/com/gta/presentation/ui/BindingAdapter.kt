63f15c8703ef6fab6e54545c5fa5be3cf2e44da4
Juyoung Jin <jinjuyoung05207@gmail.com>
2022-11-16 15:42:42 +0900

    design: 날짜 기본값 주기

--- presentation/src/main/java/com/gta/presentation/ui/BindingAdapter.kt
+++ presentation/src/main/java/com/gta/presentation/ui/BindingAdapter.kt
@@ -11,16 +11,19 @@ import java.util.*
 
 @BindingAdapter("selection", "date_type")
 fun setReservationTime(textView: TextView, selection: Pair<Long, Long>?, dateType: DateType) {
-    selection ?: return
     when(dateType) {
         DateType.RANGE -> {
-            val dateFormat = SimpleDateFormat("yy/MM/dd", Locale.getDefault())
-            textView.text = "${dateFormat.format(selection.first)} ~ ${dateFormat.format(selection.second)}"
+            textView.text = selection?.let {
+                val dateFormat = SimpleDateFormat("yy/MM/dd", Locale.getDefault())
+                "${dateFormat.format(selection.first)} ~ ${dateFormat.format(selection.second)}"
+            } ?: textView.resources.getString(R.string.placeholder_date_range)
         }
         DateType.DAY_COUNT -> {
-            val dayTimeUnit = 86400000L
-            val dayCount = (selection.second - selection.first).div(dayTimeUnit).plus(1)
-            textView.text = String.format(textView.resources.getString(R.string.total_time), dayCount)
+            textView.text = selection?.let {
+                val dayTimeUnit = 86400000L
+                val dayCount = (selection.second - selection.first).div(dayTimeUnit).plus(1)
+                String.format(textView.resources.getString(R.string.total_time), dayCount)
+            } ?: textView.resources.getString(R.string.placeholder_date_count)
         }
     }
 }
