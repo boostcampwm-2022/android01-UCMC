ea4308b01d8bfe9f740186ca1c0d5a51d1425b16
Hyunji99Choi <tyg06229@gmail.com>
2022-11-16 17:39:34 +0900

    design : 차 상세 페이지 선택 불가능 시 clickable 대신 enable 변경

--- presentation/src/main/java/com/gta/presentation/ui/BindingAdapter.kt
+++ presentation/src/main/java/com/gta/presentation/ui/BindingAdapter.kt
@@ -4,27 +4,30 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.gta.presentation.R
-import com.gta.presentation.model.carDetail.BtnType
+import com.gta.presentation.model.carDetail.UserState
 
 @BindingAdapter("car_type", "car_title")
 fun setCarDetailTitle(textView: TextView, type: String, title: String) {
     textView.text = "[$type] $title"
 }
 
-@BindingAdapter("set_car_detail_btn_state")
-fun setCarDetailBtnState(button: Button, state: BtnType) {
+@BindingAdapter("set_car_detail_button")
+fun setCarDetailBtnState(button: Button, state: UserState) {
     button.text = when (state) {
-        BtnType.OWNER -> {
+        UserState.OWNER -> {
+            button.isEnabled = true
             button.resources.getString(R.string.correction)
         }
-        BtnType.RENTED -> {
+        UserState.RENTED -> {
+            button.isEnabled = true
             button.resources.getString(R.string.extension_and_return)
         }
-        BtnType.USER -> {
+        UserState.USER -> {
+            button.isEnabled = true
             button.resources.getString(R.string.reservation)
         }
-        BtnType.NONE -> {
-            button.isClickable = false
+        UserState.NONE -> {
+            button.isEnabled = false
             button.resources.getString(R.string.reservation)
         }
     }
