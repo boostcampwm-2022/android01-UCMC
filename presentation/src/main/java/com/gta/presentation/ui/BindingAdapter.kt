1178af102e1eb2878cd12dcb2d7130b8ecdad018
Juyoung Jin <jinjuyoung05207@gmail.com>
2022-11-16 15:42:42 +0900

    design: 날짜 기본값 주기

--- presentation/src/main/java/com/gta/presentation/ui/BindingAdapter.kt
+++ presentation/src/main/java/com/gta/presentation/ui/BindingAdapter.kt
@@ -38,18 +38,19 @@ fun setCarDetailBtnState(button: Button, state: UserState) {
 
 @BindingAdapter("selection", "date_type")
 fun setReservationTime(textView: TextView, selection: Pair<Long, Long>?, dateType: DateType) {
-    selection ?: return
     when (dateType) {
         DateType.RANGE -> {
-            val dateFormat = SimpleDateFormat("yy/MM/dd", Locale.getDefault())
-            textView.text =
+            textView.text = selection?.let {
+                val dateFormat = SimpleDateFormat("yy/MM/dd", Locale.getDefault())
                 "${dateFormat.format(selection.first)} ~ ${dateFormat.format(selection.second)}"
+            } ?: textView.resources.getString(R.string.placeholder_date_range)
         }
         DateType.DAY_COUNT -> {
-            val dayTimeUnit = 86400000L
-            val dayCount = (selection.second - selection.first).div(dayTimeUnit).plus(1)
-            textView.text =
+            textView.text = selection?.let {
+                val dayTimeUnit = 86400000L
+                val dayCount = (selection.second - selection.first).div(dayTimeUnit).plus(1)
                 String.format(textView.resources.getString(R.string.total_time), dayCount)
+            } ?: textView.resources.getString(R.string.placeholder_date_count)
         }
     }
 }
