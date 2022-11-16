51ffa736f64ce6411f50539246c111c59f5d1f3b
Juyoung Jin <jinjuyoung05207@gmail.com>
2022-11-16 15:10:56 +0900

    feat: 보험 옵션 선택 이벤트 처리

--- presentation/src/main/java/com/gta/presentation/ui/Reservation/ReservationViewModel.kt
+++ presentation/src/main/java/com/gta/presentation/ui/Reservation/ReservationViewModel.kt
@@ -8,4 +8,5 @@ import javax.inject.Inject
 @HiltViewModel
 class ReservationViewModel @Inject constructor() : ViewModel() {
     val selectedDateRange = MutableLiveData<Pair<Long, Long>>()
+    val selectedInsuranceOption = MutableLiveData<Pair<Int, Int>>()
 }
