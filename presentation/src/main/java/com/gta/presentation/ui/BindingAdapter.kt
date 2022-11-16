177bd993612bdc48d2503f790a336777372878a3
Juyoung Jin <jinjuyoung05207@gmail.com>
2022-11-16 10:02:44 +0900

    fix: 예약 날짜 BindingAdatper 수정

--- presentation/src/main/java/com/gta/presentation/ui/BindingAdapter.kt
+++ presentation/src/main/java/com/gta/presentation/ui/BindingAdapter.kt
@@ -2,14 +2,25 @@ package com.gta.presentation.ui
 
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.gta.presentation.R
+import com.gta.presentation.model.DateType
+import com.gta.presentation.ui.reservation.ReservationViewModel
 import java.text.SimpleDateFormat
 import java.util.*
 
 
-@BindingAdapter("selection")
-fun setReservationTime(textView: TextView, selection: Pair<Long, Long>?) {
+@BindingAdapter("selection", "date_type")
+fun setReservationTime(textView: TextView, selection: Pair<Long, Long>?, dateType: DateType) {
     selection ?: return
-    val dateFormat = SimpleDateFormat("yy/MM/dd", Locale.getDefault())
-    textView.text = "${dateFormat.format(selection.first)} ~ ${dateFormat.format(selection.second)}"
+    when(dateType) {
+        DateType.RANGE -> {
+            val dateFormat = SimpleDateFormat("yy/MM/dd", Locale.getDefault())
+            textView.text = "${dateFormat.format(selection.first)} ~ ${dateFormat.format(selection.second)}"
+        }
+        DateType.DAY_COUNT -> {
+            val dayTimeUnit = 86400000L
+            val dayCount = (selection.second - selection.first).div(dayTimeUnit).plus(1)
+            textView.text = String.format(textView.resources.getString(R.string.total_time), dayCount)
+        }
+    }
 }
-
