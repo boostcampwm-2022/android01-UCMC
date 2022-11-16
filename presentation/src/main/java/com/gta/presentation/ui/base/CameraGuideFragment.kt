5d5e6b0fa7af8aa7c0d3abaaab5f1f93da2a2a93
minseonglove <qnfpdlr124@naver.com>
2022-11-16 15:13:55 +0900

    feat : 카메라 가이드 프레그먼트 추가

---
+++ presentation/src/main/java/com/gta/presentation/ui/base/CameraGuideFragment.kt
@@ -0,0 +1,67 @@
+package com.gta.presentation.ui.base
+
+import android.Manifest
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
+import androidx.core.content.PermissionChecker
+import com.gta.presentation.R
+import com.gta.presentation.databinding.FragmentCameraGuideBinding
+import java.io.File
+
+abstract class CameraGuideFragment(
+    private val onSuccess: (Uri) -> Unit
+) : BaseFragment<FragmentCameraGuideBinding>(R.layout.fragment_camera_guide) {
+
+    private val permissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                cameraLauncher.launch(photoUri)
+            }
+        }
+
+    private val cameraLauncher =
+        registerForActivityResult(ActivityResultContracts.TakePicture()) { isSucceed ->
+            if (isSucceed) {
+                onSuccess(photoUri)
+            }
+        }
+
+    private val photoFile by lazy {
+        File.createTempFile(
+            "IMG_",
+            ".jpg",
+            requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        )
+    }
+
+    private val photoUri by lazy {
+        FileProvider.getUriForFile(
+            requireContext(),
+            "${requireContext().packageName}.provider",
+            photoFile
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnLicenseGuidePicture.setOnClickListener {
+            takePicture()
+        }
+    }
+
+    private fun takePicture() {
+        val permissionCheck = PermissionChecker.checkCallingOrSelfPermission(
+            requireContext(),
+            Manifest.permission.CAMERA
+        )
+        if (permissionCheck == PermissionChecker.PERMISSION_GRANTED) {
+            cameraLauncher.launch(photoUri)
+        } else {
+            permissionLauncher.launch(Manifest.permission.CAMERA)
+        }
+    }
+}
