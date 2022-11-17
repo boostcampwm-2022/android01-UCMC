42455860c8536336a82f1a1cae83629961257ed0
minseonglove <qnfpdlr124@naver.com>
2022-11-17 13:54:41 +0900

    feat : 자동차 등록 가이드 화면 추가

---
+++ presentation/src/main/java/com/gta/presentation/ui/pinkslip/guide/PinkSlipGuideFragment.kt
@@ -0,0 +1,24 @@
+package com.gta.presentation.ui.pinkslip.guide
+
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.gta.presentation.R
+import com.gta.presentation.ui.MainActivity
+import com.gta.presentation.ui.base.CameraGuideFragment
+
+class PinkSlipGuideFragment : CameraGuideFragment() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as MainActivity).supportActionBar?.title =
+            getString(R.string.pink_slip_guide_toolbar)
+        binding.tvCameraGuideTitle.setText(R.string.pink_slip_guide_title)
+        binding.ivCameraGuideSample.setImageResource(R.drawable.img_pink_slip)
+    }
+
+    override fun navigate(uri: String) {
+        val direction = PinkSlipGuideFragmentDirections
+            .actionPinkSlipGuideFragmentToPinkSlipRegistrationFragment(uri)
+        findNavController().navigate(direction)
+    }
+}
