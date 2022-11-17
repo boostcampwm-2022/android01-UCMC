82b624f7f24f64f09f47f09d06b2bc2a846725fa
Hoon <ldh019@naver.com>
2022-11-17 14:39:11 +0900

    feat : Bottom Sheet Behavior, Back Pressed, 마커 색 변경

--- presentation/src/main/java/com/gta/presentation/ui/map/MapFragment.kt
+++ presentation/src/main/java/com/gta/presentation/ui/map/MapFragment.kt
@@ -2,10 +2,14 @@ package com.gta.presentation.ui.map
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.Color
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
 import android.widget.FrameLayout
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -19,14 +23,18 @@ import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.util.FusedLocationSource
+import com.naver.maps.map.util.MarkerIcons
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 
 @AndroidEntryPoint
 class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map), OnMapReadyCallback {
     private val viewModel: CarDetailViewModel by viewModels()
+
     private lateinit var naverMap: NaverMap
     private lateinit var locationSource: FusedLocationSource
+    private lateinit var backPressedCallback: OnBackPressedCallback
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<FrameLayout>
+
     private val activityResultLauncher =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { resultMap ->
             val isAllGranted = permissions.all { e -> resultMap[e] == true }
@@ -45,45 +53,33 @@ class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map), OnM
         Manifest.permission.ACCESS_COARSE_LOCATION
     )
 
-    private val bottomSheetBehavior: BottomSheetBehavior<FrameLayout> by lazy { BottomSheetBehavior.from(binding.bottomSheet) }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.mapView.onCreate(savedInstanceState)
         binding.vm = viewModel
+        binding.mapView.onCreate(savedInstanceState)
         binding.mapView.getMapAsync(this)
+
         locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+        bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheet)
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
     }
 
     override fun onMapReady(naverMap: NaverMap) {
-        this.naverMap = naverMap
-        naverMap.locationSource = locationSource
-
         activityResultLauncher.launch(permissions)
+        this.naverMap = naverMap
+        setupWithMap()
+        setupWithMarker()
+    }
 
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setupWithMap() {
+        naverMap.locationSource = locationSource
         naverMap.uiSettings.apply {
             isCompassEnabled = true
             isScaleBarEnabled = true
             isLocationButtonEnabled = true
         }
 
-        setupWithMapView()
-
-        val marker = Marker()
-        marker.position = LatLng(37.36, 127.1052)
-        marker.map = naverMap
-
-        marker.setOnClickListener {
-            Timber.d(it.toString())
-            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
-            binding.cgFilter.visibility = View.GONE
-            false
-        }
-    }
-
-    @SuppressLint("ClickableViewAccessibility")
-    private fun setupWithMapView() {
         binding.mapView.setOnTouchListener { _, event ->
             binding.cgFilter.visibility = when (event.action) {
                 MotionEvent.ACTION_MOVE -> View.GONE
@@ -93,14 +89,53 @@ class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map), OnM
         }
     }
 
+    @SuppressLint("ResourceAsColor")
+    private fun setupWithMarker() {
+        val marker = Marker()
+        marker.icon = MarkerIcons.BLACK
+        marker.iconTintColor = requireContext().getColor(R.color.primaryColor)
+        marker.position = LatLng(37.36, 127.1052)
+        marker.map = naverMap
+
+        marker.setOnClickListener {
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+            binding.cgFilter.visibility = View.GONE
+            true
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        backPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (bottomSheetBehavior.state != BottomSheetBehavior.STATE_HIDDEN) {
+                    bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                } else {
+                    requireActivity().finishAffinity()
+                }
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, backPressedCallback)
+    }
+
     override fun onStart() {
         super.onStart()
         binding.mapView.onStart()
     }
 
+    override fun onPause() {
+        binding.mapView.onPause()
+        super.onPause()
+    }
+
     override fun onResume() {
         super.onResume()
         binding.mapView.onResume()
+    }
+
+    override fun onStop() {
+        binding.mapView.onStop()
+        super.onStop()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -110,19 +145,14 @@ class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map), OnM
         super.onSaveInstanceState(outState)
     }
 
-    override fun onPause() {
-        binding.mapView.onPause()
-        super.onPause()
-    }
-
-    override fun onStop() {
-        binding.mapView.onStop()
-        super.onStop()
-    }
-
     override fun onDestroyView() {
         binding.mapView.onDestroy()
         super.onDestroyView()
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        backPressedCallback.remove()
     }
 
     override fun onLowMemory() {
@@ -131,6 +161,6 @@ class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map), OnM
     }
 
     companion object {
-        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 100
     }
 }
