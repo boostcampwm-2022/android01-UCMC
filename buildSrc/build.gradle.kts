0fa2be1dfc212d5a84d4fa5e421c47bbd276df60
Hoon <ldh019@naver.com>
2022-11-21 00:46:44 +0900

    build : Configuration, Dependencies 파일 생성 및 설정

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
-    id("java-library")
-    id("org.jetbrains.kotlin.jvm")
+    `kotlin-dsl`
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+repositories {
+    google()
+    mavenCentral()
 }
--- buildSrc/src/main/java/com/gta/buildsrc/Configuration.kt
+++ buildSrc/src/main/java/com/gta/buildsrc/Configuration.kt
@@ -1,4 +1,10 @@
 package com.gta.buildsrc
 
-class MyClass {
+object Configuration {
+    const val compileSdk = 33
+    const val buildToolsVersion = "30.0.2"
+    const val minSdk = 23
+    const val targetSdk = 33
+    const val versionCode = 1
+    const val versionName = "0.0.1"
 }
