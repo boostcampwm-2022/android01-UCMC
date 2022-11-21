5d01df64ab078d1ffa2038436a6d7015ee12d25b
minseonglove <qnfpdlr124@naver.com>
2022-11-21 22:33:34 +0900

    chore : 사용하지 않는 메소드 제거

--- data/src/main/java/com/gta/data/source/StorageDataSource.kt
+++ data/src/main/java/com/gta/data/source/StorageDataSource.kt
@@ -1,11 +1,8 @@
 package com.gta.data.source
 
 import android.net.Uri
-import android.webkit.MimeTypeMap
 import com.google.android.gms.tasks.Task
 import com.google.firebase.storage.StorageReference
-import timber.log.Timber
-import java.io.File
 import javax.inject.Inject
 
 class StorageDataSource @Inject constructor(
@@ -14,10 +11,9 @@ class StorageDataSource @Inject constructor(
     fun uploadThumbnail(uri: String): Task<Uri> {
         val image = Uri.parse(uri)
         val name = image.path?.substringAfterLast("/") ?: ""
-        getExtension(image)
         val ref = storageReference
             .child("thumb")
-            .child("${System.currentTimeMillis()}${name}")
+            .child("${System.currentTimeMillis()}$name")
         return ref.putFile(image).continueWithTask {
             ref.downloadUrl
         }
@@ -25,11 +21,4 @@ class StorageDataSource @Inject constructor(
 
     fun deleteThumbnail(path: String): Task<Void> =
         storageReference.storage.getReferenceFromUrl(path).delete()
-
-    private fun getExtension(uri: Uri) {
-        val mime = MimeTypeMap.getFileExtensionFromUrl(Uri.fromFile(File(uri.path.toString())).toString())
-        Timber.tag("mypage").i("mime $mime")
-        val extension = MimeTypeMap.getSingleton().getMimeTypeFromExtension(mime)
-        Timber.tag("mypage").i("ex $extension")
-    }
 }
