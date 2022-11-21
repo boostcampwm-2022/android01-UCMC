08359f6cb180d8e0e51dbe1e800af68adc2b49ec
Hoon <ldh019@naver.com>
2022-11-21 23:02:03 +0900

    feat : MyCarsViewModel 생성

---
+++ presentation/src/main/java/com/gta/presentation/ui/mypage/mycars/MyCarsViewModel.kt
@@ -0,0 +1,10 @@
+package com.gta.presentation.ui.mypage.mycars
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MyCarsViewModel @Inject constructor() : ViewModel() {
+    // TODO: Implement the ViewModel
+}
