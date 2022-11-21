8c9ae7ed92d0e11effbfbb4ade8b6102c7ed0f4e
Juyoung Jin <jinjuyoung05207@gmail.com>
2022-11-21 18:43:16 +0900

    feat: Car 예약 정보 가져올 유즈케이스 생성

---
+++ domain/src/main/java/com/gta/domain/usecase/GetCarRentInfoUseCase.kt
@@ -0,0 +1,12 @@
+package com.gta.domain.usecase
+
+import com.gta.domain.model.CarRentInfo
+import com.gta.domain.repository.CarRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetCarRentInfoUseCase @Inject constructor(private val repository: CarRepository) {
+    operator fun invoke(carId: String): Flow<CarRentInfo> {
+        return repository.getCarRentInfo(carId)
+    }
+}
