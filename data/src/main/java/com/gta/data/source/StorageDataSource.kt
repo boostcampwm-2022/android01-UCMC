e0b16221bd96983f63c7310a8b1c55d0166fcb59
minseonglove <qnfpdlr124@naver.com>
2022-11-21 22:31:52 +0900

    feat : 저장되는 파일명 양식 변경

--- data/src/main/java/com/gta/data/source/StorageDataSource.kt
+++ data/src/main/java/com/gta/data/source/StorageDataSource.kt
@@ -1,8 +1,11 @@
 package com.gta.data.source
 
 import android.net.Uri
+import android.webkit.MimeTypeMap
 import com.google.android.gms.tasks.Task
 import com.google.firebase.storage.StorageReference
+import timber.log.Timber
+import java.io.File
 import javax.inject.Inject
 
 class StorageDataSource @Inject constructor(
@@ -11,9 +14,10 @@ class StorageDataSource @Inject constructor(
     fun uploadThumbnail(uri: String): Task<Uri> {
         val image = Uri.parse(uri)
         val name = image.path?.substringAfterLast("/") ?: ""
+        getExtension(image)
         val ref = storageReference
             .child("thumb")
-            .child("${name}${System.currentTimeMillis()}.jpg")
+            .child("${System.currentTimeMillis()}${name}")
         return ref.putFile(image).continueWithTask {
             ref.downloadUrl
         }
@@ -21,4 +25,11 @@ class StorageDataSource @Inject constructor(
 
     fun deleteThumbnail(path: String): Task<Void> =
         storageReference.storage.getReferenceFromUrl(path).delete()
+
+    private fun getExtension(uri: Uri) {
+        val mime = MimeTypeMap.getFileExtensionFromUrl(Uri.fromFile(File(uri.path.toString())).toString())
+        Timber.tag("mypage").i("mime $mime")
+        val extension = MimeTypeMap.getSingleton().getMimeTypeFromExtension(mime)
+        Timber.tag("mypage").i("ex $extension")
+    }
 }
