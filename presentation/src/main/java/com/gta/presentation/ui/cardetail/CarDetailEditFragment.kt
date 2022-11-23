604097bbf4d22050327fe6101205eda35544ae53
Hyunji99Choi <tyg06229@gmail.com>
2022-11-23 23:29:11 +0900

    feat : 사진 추가 버튼 클릭 시 갤러리 이동

--- presentation/src/main/java/com/gta/presentation/ui/cardetail/CarDetailEditFragment.kt
+++ presentation/src/main/java/com/gta/presentation/ui/cardetail/CarDetailEditFragment.kt
@@ -1,16 +1,30 @@
 package com.gta.presentation.ui.cardetail
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import com.gta.presentation.R
 import com.gta.presentation.databinding.FragmentCarDetailEditBinding
 import com.gta.presentation.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CarDetailEditFragment : BaseFragment<FragmentCarDetailEditBinding>(
     R.layout.fragment_car_detail_edit
 ) {
+
+    private val imageResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,5 +33,23 @@ class CarDetailEditFragment : BaseFragment<FragmentCarDetailEditBinding>(
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnAddImage.setOnClickListener {
+            addImageAtGallery()
+        }
+    }
+
+    private fun addImageAtGallery() {
+        val intent = Intent(Intent.ACTION_PICK).apply {
+            setDataAndType(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                "image/*"
+            )
+        }
+        imageResult.launch(intent)
     }
 }
