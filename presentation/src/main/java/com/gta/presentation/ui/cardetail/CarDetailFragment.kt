c6ce6f49d6d51a9cdefaf730fa5862da38b388e9
Hyunji99Choi <tyg06229@gmail.com>
2022-11-24 14:47:43 +0900

    REBASE 충돌 해결

--- presentation/src/main/java/com/gta/presentation/ui/cardetail/CarDetailFragment.kt
+++ presentation/src/main/java/com/gta/presentation/ui/cardetail/CarDetailFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.gta.domain.usecase.cardetail.UseState
 import com.gta.presentation.R
 import com.gta.presentation.databinding.FragmentCarDetailBinding
 import com.gta.presentation.ui.base.BaseFragment
@@ -38,6 +39,26 @@ class CarDetailFragment : BaseFragment<FragmentCarDetailBinding>(
                         viewModel.carInfo.value.owner.id
                     )
             )
+        }
+
+        binding.btnNext.setOnClickListener {
+            when (viewModel.useState.value) {
+                UseState.OWNER -> {
+                    findNavController().navigate(
+                        CarDetailFragmentDirections
+                            .actionCarDetailFragmentToCarDetailEditFragment(
+                                viewModel.carInfo.value.id
+                            )
+                    )
+                }
+                UseState.USER -> {
+                    // TODO 예약 페이지
+                }
+                UseState.NOW_RENT_USER -> {
+                    // TODO 반납하기 페이지
+                }
+                else -> {}
+            }
         }
     }
 }
