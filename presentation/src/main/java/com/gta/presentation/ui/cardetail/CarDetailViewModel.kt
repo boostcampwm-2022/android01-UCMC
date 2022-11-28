c61379e01f38813df4b0b3be9477c56d847d1f0e
Hyunji99Choi <tyg06229@gmail.com>
2022-11-28 21:26:39 +0900

    refator : 로그인 사용자 uid를 FirebaseAuth 사용 대신 FirebaseUtil 사용

--- presentation/src/main/java/com/gta/presentation/ui/cardetail/CarDetailViewModel.kt
+++ presentation/src/main/java/com/gta/presentation/ui/cardetail/CarDetailViewModel.kt
@@ -3,11 +3,11 @@ package com.gta.presentation.ui.cardetail
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.firebase.auth.FirebaseAuth
 import com.gta.domain.model.CarDetail
 import com.gta.domain.usecase.cardetail.GetCarDetailDataUseCase
 import com.gta.domain.usecase.cardetail.GetUseStateAboutCarUseCase
 import com.gta.domain.usecase.cardetail.UseState
+import com.gta.presentation.util.FirebaseUtil
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -18,7 +18,6 @@ import javax.inject.Inject
 @HiltViewModel
 class CarDetailViewModel @Inject constructor(
     args: SavedStateHandle,
-    auth: FirebaseAuth,
     getCarDetailDataUseCase: GetCarDetailDataUseCase,
     getUseStateAboutCarUseCase: GetUseStateAboutCarUseCase
 ) : ViewModel() {
@@ -28,7 +27,7 @@ class CarDetailViewModel @Inject constructor(
 
     init {
         val carId = args.get<String>("CAR_ID") ?: "정보 없음"
-        val uid = auth.currentUser?.uid ?: "정보 없음"
+        val uid = FirebaseUtil.uid
 
         carInfo = getCarDetailDataUseCase(carId).stateIn(
             scope = viewModelScope,
