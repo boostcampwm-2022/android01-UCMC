da1c356120fe1d5e8620ebd81013e73d55f9407b
Juyoung Jin <jinjuyoung05207@gmail.com>
2022-11-29 23:18:31 +0900

    design: 반납하기 레이아웃 구현

---
+++ presentation/src/main/java/com/gta/presentation/ui/returncar/ReturnCarFragment.kt
@@ -0,0 +1,10 @@
+package com.gta.presentation.ui.returncar
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.gta.presentation.R
+import com.gta.presentation.databinding.FragmentReturnCarBinding
+import com.gta.presentation.ui.base.BaseFragment
+
+class ReturnCarFragment : BaseFragment<FragmentReturnCarBinding>(R.layout.fragment_return_car) {
+}
