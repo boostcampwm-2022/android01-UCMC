2aa1a149f8553a81ec2b0eef7c74a2ca975b2544
minseonglove <qnfpdlr124@naver.com>
2022-11-29 18:09:45 +0900

    chore : 주석 삭제

--- presentation/src/main/java/com/gta/presentation/ui/chatting/list/ChattingListFragment.kt
+++ presentation/src/main/java/com/gta/presentation/ui/chatting/list/ChattingListFragment.kt
@@ -35,41 +35,4 @@ class ChattingListFragment : BaseFragment<FragmentChattingListBinding>(
             )
         }
     }
-
-    // 채팅 채널을 만드는 메쏘드에요
-    // 이 화면에서는 필요하지 않지만 예약 완료 화면에서 이 메쏘드를 참고해 채팅방을 생성해야 하기 때문에 냅뒀어요
-    // channelId는 대여자uid-차id 에요
-    // 채널을 만들기 전에 중복 확인을 위한 queryChannel을 먼저 돌릴 필요가 있어요
-/*    private fun createChannel() {
-        chatClient.createChannel(
-            channelType = "messaging",
-            channelId = "${user.id}-차id",
-            memberIds = listOf(user.id, "상대방uid"),
-            extraData = emptyMap()
-        ).enqueue() { result ->
-            if (result.isError) {
-                Timber.tag("chatting").i(result.error().message)
-            }
-        }
-    }*/
-
-    // 채널을 만들기 전에 중복 확인을 위한 queryChannel을 먼저 돌릴 필요가 있어요
-    // 이미 채팅방이 있다면 만들어선 안되겠죠..?!
-/*    private fun checkChannel() {
-        chatClient.queryChannel(
-            "messaging",
-            "${user.id}-차id",
-            QueryChannelRequest()
-        ).enqueue { result ->
-            if (result.isSuccess) {
-                // 쿼리에 성공했다면 채널id를 받을 수 있어요
-                // 이 채널id를 들고 채팅 화면으로 넘어가면 돼요
-                Timber.tag("chatting").i(result.data().cid)
-            } else {
-                // 채널이 없다면 여기로 와요
-                // 이럴 땐 쿼리에 쓴 채널Id로 새로운 채널을 만들어 봐요
-                Timber.tag("chatting").i(result.error().message)
-            }
-        }
-    }*/
 }
