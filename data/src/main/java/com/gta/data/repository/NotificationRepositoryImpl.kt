ecbabf951e86d55dc2eb232a13d5d1a66c986538
Juyoung Jin <jinjuyoung05207@gmail.com>
2022-11-29 11:10:50 +0900

    fix: receiverToken 없으면 알림 안보냄

--- data/src/main/java/com/gta/data/repository/NotificationRepositoryImpl.kt
+++ data/src/main/java/com/gta/data/repository/NotificationRepositoryImpl.kt
@@ -9,8 +9,7 @@ import javax.inject.Inject
 
 class NotificationRepositoryImpl @Inject constructor(private val notificationDataSource: NotificationDataSource, private val userDataSource: UserDataSource) : NotificationRepository {
     override suspend fun sendNotification(notification: Notification, receiverId: String): Boolean {
-        val receiverToken = userDataSource.getUser(receiverId).first()?.messageToken ?: ""
-        // 지금 아무도 토큰 없으므로 일단 내 것
-        return notificationDataSource.sendNotification(notification, "d6GyQS2RQ5ygxIVIs9LqtH:APA91bF0THp5mqmYs6G-IYZE5O0FP2a6e7Cz-C7KLo7gZcNsNvf158uyK1QYsuf23qMQCRqVrfJtFTWteTPO6c9LNEraayb7QRIYZMykrJFGI2bxBi8nqz9B62fmLj5tvDOVG1GnDW5x")
+        val receiverToken = userDataSource.getUser(receiverId).first()?.messageToken ?: return false
+        return notificationDataSource.sendNotification(notification, receiverToken)
     }
 }
