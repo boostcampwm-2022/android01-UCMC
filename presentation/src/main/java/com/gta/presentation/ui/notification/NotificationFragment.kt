9f97f26ce5c51cde1c3ce04eaa106ed3f90eeaaf
Hoon <ldh019@naver.com>
2022-11-29 23:35:57 +0900

    feat : 딥링크 준비 위한 notification fragment 생성

---
+++ presentation/src/main/java/com/gta/presentation/ui/notification/NotificationFragment.kt
@@ -0,0 +1,14 @@
+package com.gta.presentation.ui.notification
+
+import android.os.Bundle
+import android.view.View
+import com.gta.presentation.R
+import com.gta.presentation.databinding.FragmentNotificationBinding
+import com.gta.presentation.ui.base.BaseFragment
+
+class NotificationFragment :
+    BaseFragment<FragmentNotificationBinding>(R.layout.fragment_notification) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
