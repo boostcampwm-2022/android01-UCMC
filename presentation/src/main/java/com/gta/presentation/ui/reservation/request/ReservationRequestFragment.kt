fb81cc3437c781709d1bfb14342d0f5d66f17690
Hoon <ldh019@naver.com>
2022-11-30 18:58:43 +0900

    feat : 프래그먼트 flow collect 설정

--- presentation/src/main/java/com/gta/presentation/ui/reservation/request/ReservationRequestFragment.kt
+++ presentation/src/main/java/com/gta/presentation/ui/reservation/request/ReservationRequestFragment.kt
@@ -3,35 +3,38 @@ package com.gta.presentation.ui.reservation.request
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import com.gta.presentation.R
-import com.gta.presentation.databinding.FragmentReservationBinding
+import com.gta.presentation.databinding.FragmentReservationRequestBinding
 import com.gta.presentation.ui.base.BaseFragment
+import com.gta.presentation.util.repeatOnStarted
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ReservationRequestFragment :
-    BaseFragment<FragmentReservationBinding>(R.layout.fragment_reservation) {
+    BaseFragment<FragmentReservationRequestBinding>(R.layout.fragment_reservation_request) {
 
     private val viewModel: ReservationRequestViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.getReservationEvent.collect {
-                    viewModel.carId = it.carId
-                    when (it.insuranceOption) {
-                        "LOW" -> binding.rgReservationInsuranceOption1.isChecked = true
-                        "MEDIUM" -> binding.rgReservationInsuranceOption2.isChecked = true
-                        "HIGH" -> binding.rgReservationInsuranceOption3.isChecked = true
-                        else -> {}
-                    }
+        binding.vm = viewModel
+
+        repeatOnStarted(viewLifecycleOwner) {
+            viewModel.reservation?.collect {
+                when (it.insuranceOption) {
+                    "LOW" -> binding.rgReservationInsuranceOption1.isChecked = true
+                    "MEDIUM" -> binding.rgReservationInsuranceOption2.isChecked = true
+                    "HIGH" -> binding.rgReservationInsuranceOption3.isChecked = true
+                    else -> {}
                 }
+            }
+        }
+
+        repeatOnStarted(viewLifecycleOwner) {
+            viewModel.createReservationEvent.collect {
+                findNavController().popBackStack()
             }
         }
     }
