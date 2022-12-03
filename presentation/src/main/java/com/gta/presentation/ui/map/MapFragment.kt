ecfa27f77be67e41029a256c7db92622fd572db9
Hoon <ldh019@naver.com>
2022-12-03 12:10:26 +0900

    feat : 카메라 위치 제한 & 줌 레벨 제한

--- presentation/src/main/java/com/gta/presentation/ui/map/MapFragment.kt
+++ presentation/src/main/java/com/gta/presentation/ui/map/MapFragment.kt
@@ -24,6 +24,7 @@ import com.gta.presentation.databinding.FragmentMapBinding
 import com.gta.presentation.ui.base.BaseFragment
 import com.gta.presentation.ui.mypage.mycars.OnItemClickListener
 import com.naver.maps.geometry.LatLng
+import com.naver.maps.geometry.LatLngBounds
 import com.naver.maps.map.CameraAnimation
 import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.LocationTrackingMode
@@ -107,6 +108,11 @@ class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map), OnM
     private fun setupWithMap() {
         naverMap.locationSource = locationSource
         naverMap.locationTrackingMode = mapMode
+
+        naverMap.extent = LatLngBounds(MAP_MIN_BOUND, MAP_MAX_BOUND)
+        naverMap.minZoom = MAP_MIN_ZOOM
+        naverMap.maxZoom = MAP_MAX_ZOOM
+
         naverMap.uiSettings.apply {
             isCompassEnabled = true
             isScaleBarEnabled = true
@@ -319,5 +325,11 @@ class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map), OnM
 
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+
+        private const val MAP_MIN_ZOOM = 5.0
+        private const val MAP_MAX_ZOOM = 18.0
+
+        private val MAP_MIN_BOUND = LatLng(31.43, 122.37)
+        private val MAP_MAX_BOUND = LatLng(44.35, 132.0)
     }
 }
