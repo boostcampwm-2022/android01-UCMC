f18669418121fa60174691e3d89f8e66157421d6
minseonglove <qnfpdlr124@naver.com>
2022-12-05 23:18:27 +0900

    feat : 자동차 등록 기능 예외 처리

--- data/src/main/java/com/gta/data/repository/PinkSlipRepositoryImpl.kt
+++ data/src/main/java/com/gta/data/repository/PinkSlipRepositoryImpl.kt
@@ -2,11 +2,11 @@ package com.gta.data.repository
 
 import com.gta.data.source.PinkSlipDataSource
 import com.gta.data.source.UserDataSource
+import com.gta.domain.model.DuplicatedItemException
+import com.gta.domain.model.FirestoreException
 import com.gta.domain.model.PinkSlip
+import com.gta.domain.model.UCMCResult
 import com.gta.domain.repository.PinkSlipRepository
-import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.first
 import java.nio.ByteBuffer
 import javax.inject.Inject
@@ -16,23 +16,32 @@ class PinkSlipRepositoryImpl @Inject constructor(
     private val pinkSlipDataSource: PinkSlipDataSource
 ) : PinkSlipRepository {
 
-    override fun getPinkSlip(buffer: ByteBuffer): Flow<PinkSlip?> = callbackFlow {
-        trySend(pinkSlipDataSource.getRandomPinkSlip())
-        awaitClose()
-    }
+    override fun getPinkSlip(buffer: ByteBuffer): PinkSlip =
+        pinkSlipDataSource.getRandomPinkSlip()
 
-    override fun setPinkSlip(uid: String, pinkSlip: PinkSlip): Flow<Boolean> = callbackFlow {
+    override suspend fun setPinkSlip(uid: String, pinkSlip: PinkSlip): UCMCResult<Unit> {
         /*
             1. 유저 정보 가져오기
             2. 리스트 뒤에 새로운 차의 ID 붙이고 업데이트
             3. 차 테이블에 새로운 차 추가
          */
-        userDataSource.getUser(uid).first()?.let { userInfo ->
-            val updatedCars = userInfo.myCars.plus(pinkSlip.informationNumber)
-            pinkSlipDataSource.run {
-                trySend(updateCars(uid, updatedCars).first() && createCar(uid, pinkSlip).first())
+        return userDataSource.getUser(uid).first()?.let { userInfo ->
+            if (userInfo.myCars.contains(pinkSlip.informationNumber)) {
+                UCMCResult.Error(DuplicatedItemException())
+            } else {
+                val updatedCars = userInfo.myCars.plus(pinkSlip.informationNumber)
+                updateCars(uid, updatedCars, pinkSlip)
             }
-        } ?: trySend(false)
-        awaitClose()
+        } ?: UCMCResult.Error(FirestoreException())
     }
+
+    private suspend fun updateCars(uid: String, cars: List<String>, pinkSlip: PinkSlip): UCMCResult<Unit> =
+        pinkSlipDataSource.run {
+            val result = updateCars(uid, cars).first() && createCar(uid, pinkSlip).first()
+            if (result) {
+                UCMCResult.Success(Unit)
+            } else {
+                UCMCResult.Error(FirestoreException())
+            }
+        }
 }
