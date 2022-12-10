ccaea14fc6ca43ffa8ffaa65a38aa25935d379ef
minseonglove <qnfpdlr124@naver.com>
2022-12-10 11:44:27 +0900

    feat : 테스트용 상수를 모은 TestUtil 추가

--- data/src/main/java/com/gta/data/util/EmptyUtil.kt
+++
@@ -1,3 +0,0 @@
-package com.gta.data.util
-
-class EmptyUtil
---
+++ data/src/main/java/com/gta/data/util/TestUtil.kt
@@ -0,0 +1,5 @@
+package com.gta.data.util
+
+const val GOOD_UID = "GoodDonghoon"
+const val BAD_UID = "BadDonghoon"
+const val EXCEPTION_UID = "ErrorDonghoon"
