ad9d6e734ad94b239dcd3a8826897115746cf448
Juyoung Jin <jinjuyoung05207@gmail.com>
2022-12-12 15:33:07 +0900

    feat: finishReservation merge 후 Result 감싸기

--- domain/src/main/java/com/gta/domain/usecase/reservation/FinishReservationUseCase.kt
+++ domain/src/main/java/com/gta/domain/usecase/reservation/FinishReservationUseCase.kt
@@ -1,34 +1,40 @@
 package com.gta.domain.usecase.reservation
 
+import com.gta.domain.model.FirestoreException
 import com.gta.domain.model.Notification
 import com.gta.domain.model.NotificationType
 import com.gta.domain.model.Reservation
+import com.gta.domain.model.ReservationState
 import com.gta.domain.model.UCMCResult
 import com.gta.domain.repository.ReservationRepository
 import com.gta.domain.usecase.SendNotificationUseCase
-import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 class FinishReservationUseCase @Inject constructor(
-    private val repository: ReservationRepository,
+    private val reservationRepository: ReservationRepository,
     private val notificationUseCase: SendNotificationUseCase
 ) {
     suspend operator fun invoke(
         accepted: Boolean,
         reservation: Reservation,
-        ownerId: String
-    ): Boolean {
-        val reservationId = repository.createReservation(reservation).first()
-        val notificationResult = notificationUseCase(
-            Notification(
-                type = if (accepted) NotificationType.ACCEPT_RESERVATION.title else NotificationType.DECLINE_RESERVATION.title,
-                message = if (accepted) NotificationType.ACCEPT_RESERVATION.msg else NotificationType.DECLINE_RESERVATION.msg,
-                reservationId = reservationId,
-                fromId = ownerId,
-                timestamp = System.currentTimeMillis()
-            ),
-            reservation.lenderId
+        reservationId: String
+    ): UCMCResult<Unit> {
+        val notification = Notification(
+            type = if (accepted) NotificationType.ACCEPT_RESERVATION.title else NotificationType.DECLINE_RESERVATION.title,
+            message = if (accepted) NotificationType.ACCEPT_RESERVATION.msg else NotificationType.DECLINE_RESERVATION.msg,
+            reservationId = reservationId,
+            fromId = reservation.ownerId,
+            timestamp = System.currentTimeMillis()
         )
-        return reservationId.isNotEmpty() && notificationResult is UCMCResult.Success
+
+        return if (reservationRepository.updateReservationState(
+                reservationId,
+                if (accepted) ReservationState.ACCEPT else ReservationState.CANCEL
+            ) is UCMCResult.Success
+        ) {
+            notificationUseCase(notification, reservation.lenderId)
+        } else {
+            UCMCResult.Error(FirestoreException())
+        }
     }
 }
