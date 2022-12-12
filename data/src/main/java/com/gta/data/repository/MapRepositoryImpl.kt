13e4c2860eeb798048e9716be25cd96a246323cf
hoon <ldh019@naver.com>
2022-12-12 18:04:45 +0900

    fix : MapRepository catch 로직 변경

--- data/src/main/java/com/gta/data/repository/MapRepositoryImpl.kt
+++ data/src/main/java/com/gta/data/repository/MapRepositoryImpl.kt
@@ -6,35 +6,34 @@ import com.gta.domain.model.LocationInfo
 import com.gta.domain.model.UCMCResult
 import com.gta.domain.repository.MapRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import timber.log.Timber
 import javax.inject.Inject
 
 class MapRepositoryImpl @Inject constructor(private val mapDataSource: MapDataSource) :
     MapRepository {
-    override fun getSearchAddressList(query: String): Flow<UCMCResult<List<LocationInfo>>> = flow {
-        try {
-            val addressResult = mapDataSource.getSearchAddressList(query)
-            val keywordResult = mapDataSource.getSearchKeywordList(query)
-            val result = mutableListOf<LocationInfo>()
+    override fun getSearchAddressList(query: String): Flow<UCMCResult<List<LocationInfo>>> = flow<UCMCResult<List<LocationInfo>>> {
+        val addressResult = mapDataSource.getSearchAddressList(query)
+        val keywordResult = mapDataSource.getSearchKeywordList(query)
+        val result = mutableListOf<LocationInfo>()
 
-            addressResult.documents
-                .map {
-                    it.toLocationInfo()
-                }.also {
-                    result.addAll(it)
-                }
+        addressResult.documents
+            .map {
+                it.toLocationInfo()
+            }.also {
+                result.addAll(it)
+            }
 
-            keywordResult.documents
-                .map {
-                    it.toLocationInfo()
-                }.also {
-                    result.addAll(it)
-                }
-            emit(UCMCResult.Success(result))
-        } catch (e: Exception) {
-            emit(UCMCResult.Error(e))
-        }
+        keywordResult.documents
+            .map {
+                it.toLocationInfo()
+            }.also {
+                result.addAll(it)
+            }
+        emit(UCMCResult.Success(result))
+    }.catch { e ->
+        emit(UCMCResult.Error(Exception(e.message)))
     }
 
     override fun getSearchCoordinate(longitude: String, latitude: String): Flow<String?> = flow {
