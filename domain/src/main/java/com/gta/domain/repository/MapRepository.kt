ea0d5614ebefff4ce46a9a19fff93dd8f8c36c77
Hyunji99Choi <tyg06229@gmail.com>
2022-12-12 16:04:58 +0900

    feat : 내차 수정하기 위치 수정의 위치 통신 UCMCResult 예외처리

--- domain/src/main/java/com/gta/domain/repository/MapRepository.kt
+++ domain/src/main/java/com/gta/domain/repository/MapRepository.kt
@@ -5,6 +5,6 @@ import com.gta.domain.model.UCMCResult
 import kotlinx.coroutines.flow.Flow
 
 interface MapRepository {
-    fun getSearchCoordinate(longitude: String, latitude: String): Flow<String?>
+    fun getSearchCoordinate(longitude: String, latitude: String): Flow<UCMCResult<String>>
     fun getSearchAddressList(query: String): Flow<UCMCResult<List<LocationInfo>>>
 }
